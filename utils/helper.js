bae0b18ba744be00d5752a3c2f317e805ac0a784
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2022-12-01 00:24:53 +1100

    fixing bugs

--- utils/helper.js
+++ utils/helper.js
@@ -1,8 +1,10 @@
+function formatDate(date) {
+  return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+      date
+    ).getFullYear()}`;
+}
+
+
 module.exports = {
-    format_date: (date) => {
-      return date.toLocaleDateString();
-    },
-    format_amount: (amount) => {
-      return parseInt(amount).toLocaleString();
-    },
-  };
+  formatDate
+}
