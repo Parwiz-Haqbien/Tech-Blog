aea583e3660dacfaa6d08d23947c642e4385a7ff
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2022-12-01 13:36:00 +1100

    adding post routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,2 +1,58 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Model } = require('sequelize');
+const { Post } = require('../../models');
+const useAuth = require('../../utils/auth')
+
+router.post('/', useAuth, async( req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body, 
+            user_id: req.session.user_id
+        });
+        res.redirect('/dashboard');
+    } catch(err) {
+        res.status(500).json(err)
+    }
+});
+
+router.put('/:id', useAuth, async( req, res) => {
+    try {
+        const postUpdates = await Post.update({
+            ...req.body, 
+            user_id: req.session.user_id
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        });
+       res.status(200).json([postUpdates])
+    } catch(err) {
+        res.status(500).json(err)
+    }
+});
+
+router.delete('/:id', useAuth, async( req, res) => {
+    try {
+        const deletePost = await Post.destroy({
+            ...req.body, 
+             id: req.params.id
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        });
+       res.status(200).json([deletePost])
+    } catch(err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
+
+
+
+
+  
+  
